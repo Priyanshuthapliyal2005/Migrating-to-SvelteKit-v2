ed6f0c1668221e9b57ffb8109564610cee829eb9
Priyanshu Thapliyal <priyanshuthapliyal2005@gmail.com>
2024-11-11 22:01:20 +0530

    replace throw with error complete

--- replace-throw-with-error-call/src/index.ts
+++ replace-throw-with-error-call/src/index.ts
@@ -3,6 +3,17 @@ import type { Api } from "@codemod.com/workflow";
 export async function workflow({ files }: Api) {
   await files("**/*.ts")
     .jsFam()
-    .astGrep("console.log($A)")
-    .replace("console.error($A)");
+    .astGrep({
+      rule: {
+        kind: "throw_statement",
+        pattern: "throw error($A, $B)"
+      },
+      fix: `
+        error($A, $B)
+      `
+    })
+    .replace(({ getNode, getMatch }) => {
+      console.log(getNode().text());
+      return getNode().text().replace(/^throw\s+/, "");
+    });
 }
---
+++ replace-throw-with-error-call/test/test.ts
@@ -0,0 +1,8 @@
+import { error } from '@sveltejs/kit'
+throw error(500, 'something went wrong');
+
+import { error } from '@sveltejs/kit'
+throw error(50, 'something ');
+
+import { error } from '@sveltejs/kit'
+throw error(200, 'went wrong');
